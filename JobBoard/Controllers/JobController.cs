5368195d1da2687f2d07fac6061bc8a86dd4467e
Nicholas Pfleegor <ndp5030@gmail.com>
2022-06-06 16:33:53 -0400

    Job View Added

--- JobBoard/Controllers/JobController.cs
+++ JobBoard/Controllers/JobController.cs
@@ -1,14 +1,55 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using JobBoard.Models;
+using JobBoard.ViewModels;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using JobBoard.Data;
 
 namespace JobBoard.Controllers
 {
     [Authorize]
     public class JobController : Controller
     {
+        private ApplicationDbContext context;
+
+        public JobController(ApplicationDbContext dbContext)
+        {
+            context = dbContext;
+        }
         public IActionResult Index()
+        {
+            return View("AddJob");
+        }
+
+        public IActionResult AddJob()
+        {
+            AddJobViewModel jobViewModel = new AddJobViewModel();
+            return View(jobViewModel);
+        }
+
+        [HttpPost]
+        //[Route("/add")]
+        public IActionResult ProcessAddJobForm(AddJobViewModel addJobViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                Job newJob = new Job()
+                {
+                    Name = addJobViewModel.JobTitle,
+                    Location = addJobViewModel.JobLocation, 
+                    Description = addJobViewModel.JobDescription
+                };
+
+                context.Jobs.Add(newJob);
+                context.SaveChanges();
+                return Redirect("/JobPosted");
+            }
+            return View("AddJob", addJobViewModel);
+        }
+
+        public IActionResult JobPosted()
         {
             return View();
         }
+
     }
 }
